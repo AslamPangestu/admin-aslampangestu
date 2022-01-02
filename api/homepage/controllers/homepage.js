0cf48f885d6f2e089e2a9e7720fcade3e22546e4
aslampangestu <aslampangestu@gmail.com>
2022-01-02 19:32:22 +0700

    * update home page * remove unused

--- api/homepage/controllers/homepage.js
+++ api/homepage/controllers/homepage.js
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
+// TODO: New API get all data homepage
 module.exports = {};
