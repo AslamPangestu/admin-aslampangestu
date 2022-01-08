5b124ef8c82efd84fb1a4131276ecd59ddc04fbd
aslampangestu <aslampangestu@gmail.com>
2022-01-08 18:33:53 +0700

    add new api. getall homepage

--- api/homepage/controllers/homepage.js
+++ api/homepage/controllers/homepage.js
@@ -1,9 +1,30 @@
-"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-// TODO: New API get all data homepage
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findAll() {
+    const { contact, homepage, project, skill } = strapi.models;
+
+    const contactEntity = await strapi.services.contact.find();
+    const homepageEntity = await strapi.services.homepage.find();
+    const projectEntity = await strapi.services.project.find();
+    const skillEntity = await strapi.services.skill.find();
+    const res = {
+      homepage: sanitizeEntity(homepageEntity, { model: homepage }),
+      contact: sanitizeEntity(contactEntity, { model: contact }),
+      project: sanitizeEntity(projectEntity, { model: project }),
+      skill: sanitizeEntity(skillEntity, { model: skill }),
+    };
+    return res;
+  },
+};
