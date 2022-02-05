fdba0541abbddc935e026cf1f3df5fcf75c471f0
aslampangestu <aslampangestu@gmail.com>
2022-02-06 01:03:48 +0700

    pagination article

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -7,6 +7,32 @@ module.exports = {
    * @return {Object}
    */
 
+  async find(ctx) {
+    const { page } = ctx.params;
+    const currentPage = parseInt(page);
+    const [entity, total] = await Promise.all([
+      strapi.services.article.find({
+        _sort: "updatedAt:ASC",
+        _start: (currentPage * 10).toString() || "1",
+        _limit: "10",
+      }),
+      strapi.services.article.count(),
+    ]);
+    const countTotalPage = total === "0" ? 1 : Math.ceil(parseInt(total) / 10);
+    const countPrevPage =
+      currentPage === 1 ? "1" : (currentPage - 1).toString();
+    const countNextPage =
+      page === countTotalPage ? countTotalPage : (currentPage + 1).toString();
+    return {
+      prevPage: countPrevPage,
+      currentPage: page,
+      nextPage: countNextPage,
+      totalPage: countTotalPage.toString(),
+      total,
+      data: sanitizeEntity(entity, { model: strapi.models.article }),
+    };
+  },
+
   async findOne(ctx) {
     const { slug } = ctx.params;
 
